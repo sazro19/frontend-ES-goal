429a4ef254b895bfe98dd36e7fe11c53a915a5cb
Alexander <sasha.zalesskiy@mail.ru>
2023-02-15 16:58:01 +0300

    completed filtration

--- js/main.js
+++ js/main.js
@@ -3,32 +3,56 @@ const favoriteProducts = [];
 const comparedProducts = [];
 
 document.addEventListener('DOMContentLoaded', function () {
-    document.querySelectorAll('.button-widget').forEach(button => {
-        button.addEventListener('click', toggleWidget);
-    });
+    document.querySelector('.products').addEventListener('click', toggleWidget);
     document.querySelector('.checkbox-toggler__input').addEventListener('change', changeShowHiddenCheckBox);
+    document.querySelector('.filter-buttons').addEventListener('click', switchFiltration);
 });
 
 function toggleWidget(event) {
-    let widget = getClosest(event.target, 'button-widget');
-    let product = getClosest(event.target, 'product');
-    if (widget.classList.contains('product__button-hide')) {
-        let isActive = widget.classList.contains('button-widget--active');
-        changeFilterState(product, isActive, hiddenProducts);
-        changeWidgetState(widget, isActive, true);
+    if (event.target.classList.contains('button-widget') || event.target.classList.contains('button-widget__icon')) {
+        let widget = getClosest(event.target, 'button-widget');
+        let product = getClosest(event.target, 'product');
+        if (widget.classList.contains('product__button-hide')) {
+            let isActive = widget.classList.contains('button-widget--active');
+            changeFilterState(product, isActive, hiddenProducts);
+            changeWidgetState(widget, isActive, true);
 
-        let showHiddenCheckbox = document.querySelector('.checkbox-toggler__input');
-        changeProductVisibility(product, showHiddenCheckbox.checked);
-    }
-    if (widget.classList.contains('product__button-favorite')) {
-        let isActive = widget.classList.contains('button-widget--active');
-        changeFilterState(product, isActive, favoriteProducts);
-        changeWidgetState(widget, isActive, true);
-    }
-    if (widget.classList.contains('product__button-compare')) {
-        let isActive = widget.classList.contains('button-widget--active');
-        changeFilterState(product, isActive, comparedProducts);
-        changeWidgetState(widget, isActive, false);
+            let showHiddenCheckbox = document.querySelector('.checkbox-toggler__input');
+            if (widget.classList.contains('button-widget--active')) {
+                if (showHiddenCheckbox.checked) {
+                    product.classList.add('product--show-hidden', 'product--hide');
+                } else {
+                    product.classList.remove('product--show-hidden');
+                    product.classList.add('product--hide');
+                }
+            } else {
+                product.classList.remove('product--show-hidden', 'product--hide');
+            }
+        }
+
+        if (widget.classList.contains('product__button-favorite')) {
+            let isActive = widget.classList.contains('button-widget--active');
+            changeFilterState(product, isActive, favoriteProducts);
+            changeWidgetState(widget, isActive, true);
+
+            let activatedFilterButton = document.querySelector('.filter-buttons__button--active');
+            if (activatedFilterButton.dataset.filter === 'favourite') {
+                product.classList.add('product--hide');
+                product.classList.remove('product--show-hidden');
+            }
+        }
+
+        if (widget.classList.contains('product__button-compare')) {
+            let isActive = widget.classList.contains('button-widget--active');
+            changeFilterState(product, isActive, comparedProducts);
+            changeWidgetState(widget, isActive, false);
+
+            let activatedFilterButton = document.querySelector('.filter-buttons__button--active');
+            if (activatedFilterButton.dataset.filter === 'comparison') {
+                product.classList.add('product--hide');
+                product.classList.remove('product--show-hidden');
+            }
+        }
     }
 }
 
@@ -49,40 +73,85 @@ function changeWidgetState(widget, isActive, changeIcon) {
     }
 }
 
-function changeFilterState(product, isActive, filteredProducts) {
-    if (isActive) {
-        let productIndex = hiddenProducts.indexOf(product);
+function changeFilterState(product, removeFromFiltered, filteredProducts) {
+    if (removeFromFiltered) {
+        let productIndex = filteredProducts.indexOf(product);
         filteredProducts.splice(productIndex, 1);
     } else {
         filteredProducts.push(product);
     }
 }
 
-function changeProductVisibility(product, show) {
-    if (show) {
-        product.classList.remove('product--hide');
-        product.classList.add('product--show-hidden');
-    } else {
-        product.classList.remove('product--show-hidden');
-        product.classList.add('product--hide');
-    }
-}
-
 function changeShowHiddenCheckBox(event) {
     let checkbox = event.target;
-    if (checkbox.checked) {
-        hiddenProducts.forEach(product => {
-            changeProductVisibility(product, true);
-        });
-    } else {
-        hiddenProducts.forEach(product => {
-            changeProductVisibility(product, false);
-        });
+    hiddenProducts.forEach(product => {
+        if (checkbox.checked) {
+            product.classList.add('product--show-hidden');
+        } else {
+            product.classList.remove('product--show-hidden');
+            product.classList.add('product--hide');
+        }
+    });
+}
+
+function switchFiltration(event) {
+    let button = event.target;
+    if (button.classList.contains('filter-buttons__button') && !button.classList.contains('filter-buttons__button--active')) {
+        document.querySelector('.filter-buttons__button--active').classList.remove('filter-buttons__button--active');
+        button.classList.add('filter-buttons__button--active');
+
+        switch (button.dataset.filter) {
+            case 'all':
+                document.querySelectorAll('.product').forEach(product => {
+                    if (!hiddenProducts.includes(product)) {
+                        product.classList.remove('product--hide');
+                    } else if (document.querySelector('.checkbox-toggler__input').checked) {
+                        product.classList.add('product--show-hidden');
+                    }
+                });
+                break;
+            case 'favourite':
+                favoriteProducts.forEach(product => {
+                    if (!hiddenProducts.includes(product)) {
+                        product.classList.remove('product--hide');
+                    } else if (document.querySelector('.checkbox-toggler__input').checked) {
+                        product.classList.add('product--show-hidden');
+                    }
+                });
+                document.querySelectorAll('.product').forEach(product => {
+                    if (!favoriteProducts.includes(product)) {
+                        product.classList.add('product--hide');
+                        product.classList.remove('product--show-hidden');
+                    }
+                });
+                break;
+            case 'comparison':
+                comparedProducts.forEach(product => {
+                    if (!hiddenProducts.includes(product)) {
+                        product.classList.remove('product--hide');
+                    } else if (document.querySelector('.checkbox-toggler__input').checked) {
+                        product.classList.add('product--show-hidden');
+                    }
+                });
+                document.querySelectorAll('.product').forEach(product => {
+                    if (!comparedProducts.includes(product)) {
+                        product.classList.add('product--hide');
+                        product.classList.remove('product--show-hidden');
+                    }
+                });
+                break;
+        }
     }
 }
 
 function getClosest(element, closestClass) {
-    if (element.classList.contains(closestClass)) return element;
-    while ((element = element.parentElement) && !element.classList.contains(closestClass));
-    return element;
+    if (element.classList.contains(closestClass)) {
+        return element;
+    }
+    while (element) {
+        if (element.classList.contains(closestClass)) {
+            return element;
+        }
+        element = element.parentElement;
+    }
 }
